8e288aaf004966d40da72d64ed99779fa2af73fa
zheddhe <remy.canal@live.fr>
2025-05-20 11:40:02 +0200

    Closes #1: Project initialization (package setup tuning)

--- setup.py
+++ setup.py
@@ -9,10 +9,16 @@ setup(
     author_email='remy.canal@live.fr',
     packages=find_packages(where='.'),  # find all packages in the current directory
     install_requires=[
-        'pandas>=2.0',
-        'PyYAML>=6.0',
-        'numpy>=1.24',
-        'matplotlib>=3.5'
+        'pandas>=2.2.3',
+        'PyYAML>=6.0.2',
+        'numpy>=2.2.4',
+        'matplotlib>=3.10.1',
+        'seaborn>=0.13.2',
+        'setuptools>=78.1.0',
+        'plotly>=6.0.1',
+        'bokeh>=3.7.3',
+        'pyproj>=3.7.1',
+        'requests>=2.32.3'
     ],
     include_package_data=True,
     classifiers=[
